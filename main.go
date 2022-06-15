35d6edff6da51b28977a4a32dbbe6f310d8719a7
Yannick Siewe <yannick.siewe@adorsys.com>
2022-06-15 12:28:58 +0200

    feat: can run with secure and non-secure connection feat: update user permissions

--- main.go
+++ main.go
@@ -12,12 +12,13 @@ import (
 )
 
 var (
-	tlsConfig       *tls.Config
-	defaultLogin    = os.Getenv("ADMINUSER")
-	defaultPassword = os.Getenv("ADMINPASSWORD")
-	loginName       = os.Getenv("LOGINACCOUNT")
-	loginPassword   = os.Getenv("LOGINPASSWORD")
-	mqEndpoint      = os.Getenv("MQENDPOINT")
+	tlsConfig        *tls.Config
+	defaultLogin     = os.Getenv("ADMINUSER")
+	defaultPassword  = os.Getenv("ADMINPASSWORD")
+	loginName        = os.Getenv("LOGINACCOUNT")
+	loginPassword    = os.Getenv("LOGINPASSWORD")
+	mqEndpoint       = os.Getenv("MQENDPOINT")
+	secureConnection = true
 )
 
 func handleError(err error, msg string) {
@@ -28,15 +29,39 @@ func handleError(err error, msg string) {
 	}
 }
 
-// Update user password function
+func connection() *rabbithole.Client {
+	if secureConnection == false {
+		amqpServerURL := []string{"http://", mqEndpoint, ":15672"}
+		rmqc, _ := rabbithole.NewClient(strings.Join(amqpServerURL, ""), defaultLogin, defaultPassword)
+
+		return rmqc
+	} else {
+		fmt.Println("Starting with HTTPS")
+		transport := &http.Transport{TLSClientConfig: tlsConfig}
+		amqpServerURL := []string{"https://", mqEndpoint}
+		rmqc, _ := rabbithole.NewTLSClient(strings.Join(amqpServerURL, ""), defaultLogin, defaultPassword, transport)
+
+		return rmqc
+	}
+}
+
+// Update user password and vhost Setting
 func updateuserpassword() error {
 	fmt.Println("Start password updated")
-	transport := &http.Transport{TLSClientConfig: tlsConfig}
-	amqpServerURL := []string{"https://", mqEndpoint}
-	rmqc, _ := rabbithole.NewTLSClient(strings.Join(amqpServerURL, ""), defaultLogin, defaultPassword, transport)
+	rmqc := connection()
 	resp, err := rmqc.PutUser(loginName, rabbithole.UserSettings{Password: loginPassword, Tags: "administrator"})
 	handleError(err, "Failed to Update User Password")
 	fmt.Println(resp)
+	if err != nil {
+		return err
+	}
+
+	resp2, err2 := rmqc.UpdatePermissionsIn("/", loginName, rabbithole.Permissions{Configure: ".*", Write: ".*", Read: ".*"})
+	handleError(err2, "Failed to update permission")
+	fmt.Println(resp2)
+	if err2 != nil {
+		return err2
+	}
 
 	return nil
 }
@@ -45,17 +70,31 @@ func updateuserpassword() error {
 // If the connection is successful, terminate and inform the user that their login is working
 // If the login failed, log in to the instance with the administrator's login and update the user's password
 func main() {
-	amqpServerURL := []string{"amqps://", loginName, ":", loginPassword, "@", mqEndpoint, ":5671/"}
+	amqpProtocol := "amqp://"
+	if secureConnection == true {
+		amqpProtocol = "amqps://"
+		fmt.Println("Start with secure amqps protocol")
+	}
+	amqpServerURL := []string{amqpProtocol, loginName, ":", loginPassword, "@", mqEndpoint, ":5671/"}
 	conn, err := amqp.Dial(strings.Join(amqpServerURL, ""))
 	if err == nil {
 		log.Println("Password Already Updated")
+
+		return
+	} else if conn == nil && err != nil {
+		fmt.Println(err)
+		fmt.Println("Connection Failed")
+
 		return
 	} else {
+		fmt.Println(err)
 		fmt.Println("Password Need to be update...")
 	}
+
 	defer conn.Close()
 
-	updateuserpassword()
-
-	fmt.Println("Password update success")
+	err = updateuserpassword()
+	if err == nil {
+		fmt.Println("Password update success")
+	}
 }
